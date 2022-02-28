a8fbaee60cca9cbf82cdfb139e1bc310dd85dd08
Pulkit Pahuja <pulkit.pahuja98@gmail.com>
2022-02-28 23:34:02 +0530

    minor link fix

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,2 +1,2 @@
-export const CONNECTED_LINK = "http://127.0.0.1:5000/mock_connected";
+export const CONNECTED_LINK = "http://127.0.0.1:5000/connected";
 export const DATA_LINK = "http://127.0.0.1:5000/data";
